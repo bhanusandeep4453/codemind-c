2ea06723fd80f3ad2059134730e21251777fa95d
bhanusandeep4453 <119839614+bhanusandeep4453@users.noreply.github.com>
2022-12-28 21:59:05 +0530

    #include<stdio.h> int main() {     int n,k;     scanf("%d",&n);     k=n%9;     printf("%d",k); }

--- Add_Digits.c
+++ Add_Digits.c
@@ -1,25 +1,8 @@
-#include <stdio.h>
+#include<stdio.h>
 int main()
 {
-    int n,t,r,s;
+    int n,k;
     scanf("%d",&n);
-    if (n<0) 
-    printf("%d is not a positive number.",n);
-    else
-    {
-        t=n;
-        while (t>9)
-        {
-            s=0;
-            while (t!=0)
-            {
-                r=t%10;
-                t=t/10;
-                s+=r;
-            }
-            t=s;
-        }
-        printf("%d",t);
-    }
-    return 0;
+    k=n%9;
+    printf("%d",k);
 }
